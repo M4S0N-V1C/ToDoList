cbbf77b5383b430f7b03e7edbea55d7e533149a7
M4S0N-V1C <marcomullerantonio@gmail.com>
2025-09-15 21:08:42 -0300

    Aligment

--- App.tsx
+++ App.tsx
@@ -3,18 +3,32 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.background}>
+      <View style={styles.container}>
+      <Text style={styles.font}>Lista de Tarefas</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
+    backgroundColor: '#7A4A9E',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
+  },
+ container: {
+    width: 345,
+    height: 48,
+    top: 54,
+    left: 24,},
+
+  font: {
+    fontFamily: 'Inter',
+    fontWeight: 'bold',
+    fontSize: 24,
+    color: '#f0f0f0',
+    
   },
 });
+ 
